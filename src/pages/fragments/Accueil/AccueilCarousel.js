7b6eebbb1f99ba6a442671a5e9aef9293ff278c0
Arthur <arthur@mondon.pro>
2023-11-23 17:40:34 +0100

    make responsive carousel

--- src/pages/fragments/Accueil/AccueilCarousel.js
+++ src/pages/fragments/Accueil/AccueilCarousel.js
@@ -10,13 +10,36 @@ function AccueilCarousel() {
     const { translations } = useLanguage();
     // Configuration de base pour le carousel
     const settings = {
-        dots: true,
+        dots: false,
         infinite: true,
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
         nextArrow: <SampleNextArrow />,
         prevArrow: <SamplePrevArrow />,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     };
 
     return (
