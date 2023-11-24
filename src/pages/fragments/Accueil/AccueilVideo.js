541beda7383188bbdc5e603a2e6e234759ec576d
Arthur <arthur@mondon.pro>
2023-11-24 16:39:25 +0100

    add margin on homescreen video

--- src/pages/fragments/Accueil/AccueilVideo.js
+++ src/pages/fragments/Accueil/AccueilVideo.js
@@ -6,11 +6,12 @@ function AccueilVideo() {
 
     return (
         <section style={styles.event_section} className={"fc f-c"}>
-                <div style={{aspectRatio:'16/9',width:'100%',maxWidth:'600px', backgroundColor:'grey', borderRadius:'2rem', overflow:'hidden'}}>
+                <div style={{aspectRatio:'16/9',width:'calc(100% - 4rem)',maxWidth:'600px', backgroundColor:'grey', borderRadius:'2rem', overflow:'hidden'}}>
                     <iframe width="100%" height="100%" src="https://www.youtube.com/embed/aVlMeOh6gHk?si=_C7PAQZ6OQTWprE5" title="YouTube video player" frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen></iframe>
                 </div>
+
 
         </section>
     );
