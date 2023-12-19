ae45671286f7f084d0b185a1303f0f6798334a40
Arthur <arthur@mondon.pro>
2023-12-19 16:25:33 +0100

    some edits on pistes.json

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer({marginTop = "0"}) {
 
     return (
         <footer style={{ ...styles.footer, marginTop: marginTop }} className={"fc f-c"}>
-            <a target={"_blank"} href={"https://www.agsformule1.com/"}><img src="logo_white.png" alt="Logo" style={styles.logo} /></a>
+            <a target={"_blank"} href={"https://www.agsformule1.com/"}><img src="logo_ags_white_old.png" alt="Logo" style={styles.logo} /></a>
             <LanguageSelector/>
             <ul style={styles.footerNavList}>
                 <li>
@@ -74,7 +74,8 @@ const styles = {
         fontWeight:'lighter',
     },
     logo: {
-        maxWidth: '205px', // Ajustez selon la taille de votre logo
+        maxWidth: '500px', // Ajustez selon la taille de votre logo
+        width: '90%', // Ajustez selon la taille de votre logo
         maxHeight: '100%', // Ajustez si nécessaire
         marginLeft: '10px', // Espace à gauche
     },
